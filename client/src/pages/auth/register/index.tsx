10752c931d4f46a6b628759b58c7be86449e59c3
divyanshu <adwait7830@gmail.com>
2024-03-30 15:32:42 +0530

    extra code removed

--- client/src/pages/auth/register/index.tsx
+++ client/src/pages/auth/register/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import { log } from 'console';
 
 const Register = () => {
     const navigate = useNavigate();
